40ab41392093f7e16de193ad2e5b1e054e4bc45a
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-03-02 22:11:49 +0300

    errors from pipline console fixed

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import requests
+import docstring
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
 student_id = os.getenv("USER_CABINET_ID")
 
-
 # CABINET MIEM INFO
 answer = requests.get(f"https://cabinet.miem.hse.ru/public"
-                      f"-api/student_statistics/{student_id}").json()
+                      f"-api/student_statistics/{student_id}", timeout=1).json()
 print(answer)
