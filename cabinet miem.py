1da57ae83d2eb5b2665169611c12dddadb2e0816
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-03-02 21:51:38 +0300

    comment update

--- cabinet miem.py
+++ cabinet miem.py
@@ -7,7 +7,7 @@ load_dotenv()
 student_id = os.getenv("USER_CABINET_ID")
 
 
-#CABINET MIEM INFO
+# CABINET MIEM INFO
 answer = requests.get(f"https://cabinet.miem.hse.ru/public"
                       f"-api/student_statistics/{student_id}").json()
 print(answer)
