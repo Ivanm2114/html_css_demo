f36f0452066e336362c61e1549284f96e9473cc4
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-03-02 19:30:47 +0300

    cabinet miem commit plus requirements.txt

---
+++ cabinet miem.py
@@ -0,0 +1,12 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+student_id = os.getenv("USER_CABINET_ID")
+
+
+#CABINET MIEM INFO
+answer = requests.get(f"https://cabinet.miem.hse.ru/public-api/student_statistics/{student_id}").json()
+print(answer)
