ae31a16734dd50d1a64dc99e9c24ce1fb52b1a30
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-03-03 16:50:53 +0300

    work with file added

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 student_id = os.getenv("USER_CABINET_ID")
-
+file = open("answer.txt", mode='w',encoding='utf-8')
 # CABINET MIEM INFO
 answer = requests.get(f"https://cabinet.miem.hse.ru/public"
                       f"-api/student_"
-                      f"statistics/{student_id}", timeout=1).json()
+                      f"statistics/{student_id}", timeout=1).text
 print(answer)
+file.write(answer)
+file.close()
