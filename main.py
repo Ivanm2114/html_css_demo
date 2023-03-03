ac0480179dd053ded7721c04f9b3e018a54e0d48
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-03-03 16:52:36 +0300

    all errors corrected

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 student_id = os.getenv("USER_CABINET_ID")
-file = open("answer.txt", mode='w',encoding='utf-8')
+file = open("answer.txt", mode='w', encoding='utf-8')
 # CABINET MIEM INFO
 answer = requests.get(f"https://cabinet.miem.hse.ru/public"
                       f"-api/student_"
