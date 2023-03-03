ef07e427595340861551122fbc95138614bfdb9c
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-03-03 20:36:23 +0300

    Russian language for cabinet miem done

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import requests
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 student_id = os.getenv("USER_CABINET_ID")
@@ -13,7 +12,7 @@ key = os.getenv("STEAM_API_KEY")
 response = requests.get(f"https://cabinet.miem.hse.ru/public"
                         f"-api/student_"
                         f"statistics/{student_id}", timeout=1).text
-response=bytes(response,"utf-8").decode("unicode_escape")
+response = bytes(response, "utf-8").decode("unicode_escape")
 
 print(response)
 
