8e2acc59d24e401d3fcd1d3ff263ecf572dd80fa
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-03-03 20:59:56 +0300

    Errors fixed

--- main.py
+++ main.py
@@ -14,14 +14,11 @@ RESPONSE = requests.get(f"https://cabinet.miem.hse.ru/public"
                         f"statistics/{student_id}", timeout=1).text
 RESPONSE = bytes(RESPONSE, "utf-8").decode("unicode_escape")
 
-
-
 # GENRAL STATS
 steam_stats = requests.get(
     f"http://api.steampowered.com/"
     f"ISteamUser/GetPlayerSummaries/"
     f"v0002/?key={key}&steamids={player_id}", timeout=1).text
-
 
 # CS:GO
 cs_stats = requests.get(
@@ -29,17 +26,15 @@ cs_stats = requests.get(
     f"ISteamUserStats/GetUserStatsForGame/"
     f"v0002/?appid=730&key={key}&steamid={player_id}", timeout=1).text
 
-
 # PAYDAY 2
 payday_stats = requests.get(
     f"http://api.steampowered.com/"
     f"ISteamUserStats/GetUserStatsForGame/"
     f"v0002/?appid=218620&key={key}&steamid={player_id}", timeout=1).text
 
-
 arr = [RESPONSE, steam_stats, cs_stats, payday_stats]
 with open("answer.txt", mode='w', encoding='utf-8') as file:
     # CABINET MIEM INFO
     for el in arr:
-        file.write(el + '\n'+"-"*100+'\n')
-        print(el + '\n'+"-"*100+'\n')
+        file.write(el + '\n' + "-" * 100 + '\n')
+        print(el + '\n' + "-" * 100 + '\n')
