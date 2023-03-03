e49f9a8e77c98957b791bd98a76bc11a6ca9cf14
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-03-03 20:58:28 +0300

    Delimiters into files and prints added

--- main.py
+++ main.py
@@ -14,31 +14,32 @@ RESPONSE = requests.get(f"https://cabinet.miem.hse.ru/public"
                         f"statistics/{student_id}", timeout=1).text
 RESPONSE = bytes(RESPONSE, "utf-8").decode("unicode_escape")
 
-print(RESPONSE)
+
 
 # GENRAL STATS
 steam_stats = requests.get(
     f"http://api.steampowered.com/"
     f"ISteamUser/GetPlayerSummaries/"
     f"v0002/?key={key}&steamids={player_id}", timeout=1).text
-print(steam_stats)
+
 
 # CS:GO
 cs_stats = requests.get(
     f"http://api.steampowered.com/"
     f"ISteamUserStats/GetUserStatsForGame/"
     f"v0002/?appid=730&key={key}&steamid={player_id}", timeout=1).text
-print(cs_stats)
+
 
 # PAYDAY 2
 payday_stats = requests.get(
     f"http://api.steampowered.com/"
     f"ISteamUserStats/GetUserStatsForGame/"
     f"v0002/?appid=218620&key={key}&steamid={player_id}", timeout=1).text
-print(payday_stats)
+
 
 arr = [RESPONSE, steam_stats, cs_stats, payday_stats]
 with open("answer.txt", mode='w', encoding='utf-8') as file:
     # CABINET MIEM INFO
     for el in arr:
-        file.write(el + '\n')
+        file.write(el + '\n'+"-"*100+'\n')
+        print(el + '\n'+"-"*100+'\n')
