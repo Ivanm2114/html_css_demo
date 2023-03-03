9684d025b8e708457bd78c7d78949b26502dd0e0
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-03-03 16:55:17 +0300

    using 'with' for resource-allocating operations

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 student_id = os.getenv("USER_CABINET_ID")
-file = open("answer.txt", mode='w', encoding='utf-8')
-# CABINET MIEM INFO
-answer = requests.get(f"https://cabinet.miem.hse.ru/public"
-                      f"-api/student_"
-                      f"statistics/{student_id}", timeout=1).text
-print(answer)
-file.write(answer)
-file.close()
+with open("answer.txt", mode='w', encoding='utf-8') as file:
+    # CABINET MIEM INFO
+    answer = requests.get(f"https://cabinet.miem.hse.ru/public"
+                          f"-api/student_"
+                          f"statistics/{student_id}", timeout=1).text
+    print(answer)
+    file.write(answer)
